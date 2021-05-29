db3cf514203ee03e0aedb21b650604c8a3a5858e
Patrick Leal <80593781+Letrick@users.noreply.github.com>
2021-05-28 21:17:57 -0300

    Add files via upload

---
+++ atv 1 - maior que.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+/**
+1 ) Escreva um programa em C++ que leia um número e o imprima caso ele seja
+
+maior que 20. 
+*/
+using namespace std;
+
+int main () {
+	int numero = 0;
+	
+	
+cout << "digite um numero inteiro: \n";
+cin >> numero;
+	
+	if (numero > 20){
+		cout << "numero: " << numero;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
